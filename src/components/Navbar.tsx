d17915c7e24977333aeebe29ca43eed4ac862bcc
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-08-15 14:00:04 +0200

    navtext

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
-('import navimg from "*.png"');
+import navimg from "../assets/navimg.png";
 
 const Navbar = () => {
   return (
     <>
       <nav className="navbar">
-        <img src="" alt="Navbar background image" />
+        <img
+          className="navbar-img"
+          src={navimg}
+          alt="Navbar background image"
+        />
+        <span className="nav-text">Welcome to my portfolio</span>
       </nav>
     </>
   );
