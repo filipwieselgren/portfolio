a71c9070a4862245515054659c6495a016e0f588
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-08-15 13:15:14 +0200

    added font

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
