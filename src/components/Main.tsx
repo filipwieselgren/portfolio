24d2695cf8e5dcbe676f9a934ede27d2aeeae4fc
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-08-23 22:39:10 +0200

    remove

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,16 +3,13 @@ import { useEffect, useState } from "react";
 import { IProjects } from "../models/IProjects";
 
 const api = "https://api.github.com/users/filipwieselgren/repos";
-const apiLang = "https://api.github.com/repos/filipwieselgren/Agile/languages";
+
 const Main = () => {
   const [projects, setProjects] = useState<IProjects[]>([]);
-  const [lang, setLang] = useState<string>("");
+
   useEffect(() => {
     axios.get<IProjects[]>(api).then((response) => {
       setProjects(response.data);
-    });
-    axios.get(apiLang).then((response) => {
-      setLang(response.data);
     });
   }, []);
 
