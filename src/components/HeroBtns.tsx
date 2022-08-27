37c35ab994490e0cf22969cf76d09fe1ffff1487
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-08-27 13:17:31 +0200

    text

--- src/components/HeroBtns.tsx
+++ src/components/HeroBtns.tsx
@@ -3,7 +3,7 @@ const HeroBtns = () => {
     <>
       <div className="btn-container">
         <button className="btn-hero">See my work</button>
-        <button className="btn-hero">Download my CV</button>
+        <button className="btn-hero">See my resume</button>
         <button className="btn-hero">Read more abot me</button>
         <button className="btn-hero">Contact me</button>
       </div>
