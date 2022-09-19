8137d3ecdbcc4372206577781bc7f6aff452d6fe
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-09-20 01:37:41 +0200

    navbar fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import me from "../assets/me.png";
+import github from "../assets/github.png";
+import linkedin from "../assets/Linkedin-logo-icon-png.png";
 
 const Navbar = () => {
   const [active, setActive] = useState<string>("nav-menu");
@@ -15,22 +17,43 @@ const Navbar = () => {
       : setToggleIcon("nav-toggler");
   };
 
+  const meInActiveNav = (
+    <>
+      <img className="active-navbar-img" src={me} alt="Navbar image of me" />
+      <div className="img-underline-active"></div>
+    </>
+  );
+  const gitLinkedActive = (
+    <>
+      <div className="some-con">
+        <img className="active-navbar-img" src={github} alt="Github" />
+        <img
+          className="active-navbar-img"
+          src={linkedin}
+          alt="Navbar image of me"
+        />
+      </div>
+    </>
+  );
+
   return (
     <>
       <nav className="navbar">
         <img className="navbar-img" src={me} alt="Navbar image of me" />
-
         <div className="name-nav-options-container">
           <div className="nav-menu-container">
             <ul className={active}>
+              {active !== "nav-menu" ? meInActiveNav : <></>}
               <li className="nav-item">Read more about me</li>
               <li className="nav-item">See my resume</li>
               <li className="nav-item">Contact me</li>
+              {active !== "nav-menu" ? gitLinkedActive : <></>}
             </ul>
+
             <div onClick={navToggle} className={toggleIcon}>
-              <div onClick={navToggle} className="line1"></div>
-              <div onClick={navToggle} className="line2"></div>
-              <div onClick={navToggle} className="line3"></div>
+              <div className="line1"></div>
+              <div className="line2"></div>
+              <div className="line3"></div>
             </div>
           </div>
 
