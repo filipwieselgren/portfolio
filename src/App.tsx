f0f0f27cf17d9914ca37b66dc09a2921f982f0c6
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-09-20 23:41:11 +0200

    removed items

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useEffect } from "react";
 import "./App.css";
 import Hero from "./components/Hero";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
-import { getFromLocal } from "./local";
 
 function App() {
   return (
