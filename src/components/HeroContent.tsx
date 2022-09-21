f81a3c56a811b9d5f934e0f93f5eb5dfcafd3ee2
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-09-21 12:02:25 +0200

    show right skill algorithm

--- src/components/HeroContent.tsx
+++ src/components/HeroContent.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import { LOADIPHLPAPI } from "dns";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { IState } from "../redux/reducers/reducer";
 
@@ -7,7 +8,34 @@ export const HeroContent = () => {
     (state: IState) => state.changeLanguage.languages
   );
 
+  let arr: string[] = ["JS", "TS", "Node", "HTML", "CSS"];
+
   let language = useSelector((state: IState) => state.changeLanguage.value);
+
+  const [skills, setSkills] = useState(arr);
+
+  const sortArray = () => {
+    let getIndexZero = skills.filter((a, index) => {
+      if (index == 0) return a;
+    });
+    let getIndexOne = skills.filter((a, index) => {
+      if (index == 1) return a;
+    });
+
+    // Add index 1 to index 0
+    skills.splice(0, 0, getIndexOne.toString());
+
+    // Remove index 1
+    skills.splice(1, 1);
+
+    // Add index 0 to last getIndex
+    skills.splice(6, 0, getIndexZero.toString());
+
+    // Remove index 0
+    skills.splice(0, 1);
+    setSkills([...skills]);
+  };
+  // console.log("skills:", skills);
 
   return (
     <>
@@ -20,6 +48,10 @@ export const HeroContent = () => {
           <></>
         );
       })}
+
+      <button onClick={sortArray}>Show skills</button>
+
+      {<div>{skills[0]}</div>}
     </>
   );
 };
