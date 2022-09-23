0d6ba34a5f79a118f8ba1c38aabda2d642d28535
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-09-23 10:23:52 +0200

    css

--- src/components/NavBtnsHero.tsx
+++ src/components/NavBtnsHero.tsx
@@ -4,17 +4,19 @@ import { Link } from "react-router-dom";
 export const NavBtnsHero = () => {
   return (
     <div className="navbts-hero-container">
-      <BsChevronCompactUp className="arrow arrow-up" />
+      <div className="hero-btn-container">
+        <BsChevronCompactUp className="arrow arrow-up" />
+        <Link to={"/about"} className="hero-link to-aboutme-link">
+          Read more about me
+        </Link>
+      </div>
 
-      <Link to={"/about"} className="hero-link to-aboutme-link">
-        Read more about me
-      </Link>
-
-      <Link to={"/about"} className="hero-link to-project-link">
-        Click or scroll to see my work
-      </Link>
-
-      <BsChevronCompactDown className="arrow arrow-down" />
+      <div className="hero-btn-container">
+        <Link to={"/about"} className="hero-link to-project-link">
+          Click or scroll to see my work
+        </Link>
+        <BsChevronCompactDown className="arrow arrow-down" />
+      </div>
     </div>
   );
 };
