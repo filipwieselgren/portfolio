580bb75b99f08091759aee3947fd0af8a4a6437c
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-09-23 13:05:30 +0200

    arrows

--- src/components/NavBtnsHero.tsx
+++ src/components/NavBtnsHero.tsx
@@ -1,4 +1,9 @@
-import { BsChevronCompactDown, BsChevronCompactUp } from "react-icons/bs/";
+import {
+  BsChevronCompactDown,
+  BsChevronCompactUp,
+  BsCaretUp,
+  BsCaretDown,
+} from "react-icons/bs/";
 import { Link } from "react-router-dom";
 
 export const NavBtnsHero = () => {
@@ -6,7 +11,7 @@ export const NavBtnsHero = () => {
     <div className="navbts-hero-container">
       <div className="hero-btn-content-container">
         <div className="hero-btn-container to-about">
-          <BsChevronCompactUp className="arrow arrow-up" />
+          <BsCaretUp className="arrow arrow-up" />
           <Link to={"/about"} className="hero-link to-aboutme-link">
             Read more about me
           </Link>
@@ -16,7 +21,7 @@ export const NavBtnsHero = () => {
           <Link to={"/about"} className="hero-link to-project-link">
             Click or scroll to see my projects
           </Link>
-          <BsChevronCompactDown className="arrow arrow-down" />
+          <BsCaretDown className="arrow arrow-down" />
         </div>
       </div>
     </div>
