f5b3e3bc31569f1d1c2d787b31d68d2c315a840d
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-09-25 22:32:07 +0200

    css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,16 +36,16 @@ const Navbar = () => {
         /> */}
 
         <div className="name-nav-options-container">
+          <div className="logo-cirkle">
+            <div className="logo-w">w</div>
+            <div className="logo-f">f</div>
+          </div>
           <div className="nav-menu-container">
             <HamburgerNav
               navToggle={navToggle}
               active={active}
               toggleIcon={toggleIcon}
             />
-            <div className="logo-cirkle">
-              <div className="logo-w">w</div>
-              <div className="logo-f">f</div>
-            </div>
           </div>
 
           <div className="navbar-name-container">
