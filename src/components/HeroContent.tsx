8a0e0afc397694e272f729f8d488c4c7a264aedc
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-09-27 14:00:26 +0200

    text

--- src/components/HeroContent.tsx
+++ src/components/HeroContent.tsx
@@ -75,7 +75,7 @@ export const HeroContent = () => {
             {/* </div> */}
             <div className="presentation-text">
               {lang.maintext}
-              <span className="swith-card">{lang.askvisitor}</span>
+              <span className="swith-card">{lang.askvisitor}.</span>
             </div>
           </div>
         ) : (
