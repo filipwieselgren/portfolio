4783837ddb4da3f41bd7d7ad2916bd736de5fbc2
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-09-28 21:58:22 +0200

    css

--- src/components/NavBtnsHero.tsx
+++ src/components/NavBtnsHero.tsx
@@ -10,19 +10,21 @@ import { Link } from "react-router-dom";
 export const NavBtnsHero = () => {
   return (
     <div className="navbts-hero-container">
-      <Link to={"/about"} className="hero-link to-aboutme-link">
-        <div className="hero-btn-container to-about">
-          Read more about me
-          <BsCaretUp className="arrow arrow-up" />
-        </div>
-      </Link>
+      <div className="hero-btn-content-container">
+        <Link to={"/about"} className="hero-link to-aboutme-link">
+          <div className="hero-btn-container to-about">
+            Read more about me
+            <BsCaretUp className="arrow arrow-up" />
+          </div>
+        </Link>
 
-      <Link to={"/projects"} className="hero-link to-project-link">
-        <div className="hero-btn-container to-projects">
-          See my projects
-          <BsCaretDown className="arrow arrow-down" />
-        </div>
-      </Link>
+        <Link to={"/projects"} className="hero-link to-project-link">
+          <div className="hero-btn-container to-projects">
+            See my projects
+            <BsCaretDown className="arrow arrow-down" />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
