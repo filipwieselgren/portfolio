3e4fd19c3d874f068ab436ca1086f519b49e5d4a
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-09-30 09:21:34 +0200

    added file

--- src/pages/HeroPage.tsx
+++ src/pages/HeroPage.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import { Projets } from "../components/Projets";
 
 export const HeroPage = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Projets />
     </>
   );
 };
