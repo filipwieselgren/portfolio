510c0f308627529c423af8ad10982f25ae492390
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-09-30 09:22:38 +0200

    added route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { HeroPage } from "./pages/HeroPage";
 import { AboutPage } from "./pages/AboutPage";
+import { Projets } from "./components/Projets";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HeroPage />}></Route>
           <Route path="/about" element={<AboutPage />}></Route>
+          <Route path="/projects" element={<Projets />}></Route>
           {/* <Route path="/prodjects" element={<FooterPage />}></Route> */}
         </Routes>
       </BrowserRouter>
