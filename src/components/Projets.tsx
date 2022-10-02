de407941950d071007c80b8141024fdc487d42e2
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-10-02 19:45:19 +0200

    projects

--- src/components/Projets.tsx
+++ src/components/Projets.tsx
@@ -1,3 +1,78 @@
 export const Projets = () => {
-  return <></>;
+  // const [projects, setProjects] = useState<IProjects[]>([]);
+
+  const projects = [
+    {
+      id: 1,
+      name: "webshop",
+      html_url: "https://webshop-seven.vercel.app/",
+      description: "A webshop for movies build with react and typescript",
+      topics: [
+        "/static/media/react.svg.dff8c2d70a3a8ec4026e.png",
+        "/static/media/Typescript_logo_2020.svg.f11fa7a0c5593ab7cb15.png",
+        "/static/media/css-logo.f84f23c3069a2d6dad20.png",
+      ],
+    },
+    {
+      id: 2,
+      name: "Todo-app",
+      html_url: "https://todo-eta-six.vercel.app/",
+      description: "My first project in JS. A todo-app",
+      topics: [
+        "/static/media/js-logo.7bab731c14a3382b0ef3.png",
+        "/static/media/sass.logo.b795e8df3332ef356023.png",
+      ],
+    },
+  ];
+
+  // useEffect(() => {
+  //   axios.get<IProjects[]>(api).then((response) => {
+  //     setProjects(response.data);
+  //   });
+  // }, []);
+
+  // const showProject = projects.map((project) => {
+  //   return (
+  //     <div key={project.id}>
+  //       <>
+  //         <h2>{project.name}</h2>
+  //         <div>{project.description}</div>
+  //         {project.topics.map((pt) => {
+  //           <div>{pt}</div>;
+  //         })}
+  //       </>
+  //     </div>
+  //   );
+  // });
+
+  const t = projects.map((project) => {
+    <div> {project.topics}</div>;
+  });
+
+  return (
+    <div className="main-wrapper">
+      {projects.map((project) => {
+        return (
+          <div key={project.id}>
+            <h2>{project.name}</h2>
+            <div>{project.description}</div>
+            <a className="link-to-p" href={project.html_url} target="_blank">
+              Test it
+            </a>
+            <h5>Build with</h5>
+            {project.topics.map((pt) => {
+              return (
+                <>
+                  <img className="project-img" src={pt} />
+                </>
+              );
+            })}
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
+
+export default Projets;
