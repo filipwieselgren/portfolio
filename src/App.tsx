083a0cfa6e542f007d45b26811f8e0e465c1fe02
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-10-03 17:48:30 +0200

    css

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./css/navbar/navbarDesktop.css";
 import "./css/hero/heroMobile.css";
 import "./css/hero/heroDesktop.css";
 import "./css/projects/projectsMobile.css";
-import "./css/projects/projectsDesktop.css";
+// import "./css/projects/projectsDesktop.css";
 import { HeroPage } from "./pages/HeroPage";
 import { AboutPage } from "./pages/AboutPage";
 import { Projets } from "./components/Projets";
