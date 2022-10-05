b3d593760e15c0fcf89e04e87e7684a3b4385b68
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-10-05 15:36:29 +0200

    css

--- src/components/Projets.tsx
+++ src/components/Projets.tsx
@@ -11,54 +11,56 @@ export const Projets = () => {
   let language = useSelector((state: IState) => state.changeLanguage.value);
 
   return (
-    <div id="projects" className="project-wrapper">
-      {languageArray.map((lang) => {
-        return lang.language === language ? (
-          lang.projects.map((p) => {
-            return (
-              <div key={p.id} className="project-card-container">
-                <img src={p.img} alt="Image on the project" />
-                <div className="p-header-container">
-                  <h3 className="project-header">{p.name}</h3>
-                </div>
-                <div className="p-discription-container">
-                  <div className="project-discription">{p.description}</div>
-                </div>
-                <div className="built-with-container">
-                  <div className="built-with-txt">{p.buildTxt}</div>
-                </div>
-                <>
-                  {p.topics.map((t) => {
-                    <img
-                      key={t.topicid}
-                      src={t.language}
-                      alt="Logo of programming language"
-                    />;
-                  })}
-                </>
+    <>
+      <div id="projects" className="project-wrapper">
+        {languageArray.map((lang) => {
+          return lang.language === language ? (
+            lang.projects.map((p) => {
+              return (
+                <div key={p.id} className="project-card-container">
+                  <img src={p.img} alt="Image on the project" />
+                  <div className="p-header-container">
+                    <h3 className="project-header">{p.name}</h3>
+                  </div>
+                  <div className="p-discription-container">
+                    <div className="project-discription">{p.description}</div>
+                  </div>
+                  <div className="built-with-container">
+                    <div className="built-with-txt">{p.buildTxt}</div>
+                  </div>
+                  <>
+                    {p.topics.map((t) => {
+                      <img
+                        key={t.topicid}
+                        src={t.language}
+                        alt="Logo of programming language"
+                      />;
+                    })}
+                  </>
 
-                <div className="project-btns-container">
-                  <a href="#projects" className="hero-link to-project-link">
-                    <div className="project-btn test-app-btn">
-                      <BsCaretRight />
-                      Test app
-                    </div>
-                  </a>
-                  <a href="#projects" className="hero-link to-project-link">
-                    <div className="project-btn see-code-btn">
-                      <BsCaretRight />
-                      See code
-                    </div>
-                  </a>
+                  <div className="project-btns-container">
+                    <a href="#projects" className="hero-link to-project-link">
+                      <div className="project-btn test-app-btn">
+                        <BsCaretRight />
+                        Test app
+                      </div>
+                    </a>
+                    <a href="#projects" className="hero-link to-project-link">
+                      <div className="project-btn see-code-btn">
+                        <BsCaretRight />
+                        See code
+                      </div>
+                    </a>
+                  </div>
                 </div>
-              </div>
-            );
-          })
-        ) : (
-          <></>
-        );
-      })}
-    </div>
+              );
+            })
+          ) : (
+            <></>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
