9db52eb7502499184b062bbbdd90607ad4a29b2f
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-10-19 21:02:23 +0200

    css

--- src/components/ Contact.tsx
+++ src/components/ Contact.tsx
@@ -1,8 +1,19 @@
+import { FaLinkedinIn } from "react-icons/fa/";
+
 export const Contact = () => {
   return (
     <section className="contact-section">
-      <div className="contact-container">
+      <div className="contact-container-container">
         <h1 className="contact-title">I would love to stay in touch</h1>
+        <div className="contact-icons">
+          <div className="icon-circle"></div>
+          <div className="icon-circle"></div>
+          <div className="icon-circle">
+            <FaLinkedinIn className="icon" />
+          </div>
+          <div className="icon-circle"></div>
+          <div className="icon-circle"></div>
+        </div>
       </div>
     </section>
   );
