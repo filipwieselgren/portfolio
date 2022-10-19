50c1544d6f28a08257fcb3ca6f53032fbed33fc5
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-10-19 21:14:12 +0200

    icons added

--- src/components/ Contact.tsx
+++ src/components/ Contact.tsx
@@ -1,18 +1,29 @@
-import { FaLinkedinIn } from "react-icons/fa/";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa/";
+import { GoMail } from "react-icons/go";
+import { TiPhone } from "react-icons/ti";
+import { TbCoffee } from "react-icons/tb";
 
 export const Contact = () => {
   return (
     <section className="contact-section">
       <div className="contact-container-container">
         <h1 className="contact-title">I would love to stay in touch</h1>
-        <div className="contact-icons">
-          <div className="icon-circle"></div>
-          <div className="icon-circle"></div>
+        <div className="contact-icons-container">
+          <div className="icon-circle">
+            <GoMail />
+          </div>
+          <div className="icon-circle">
+            <TiPhone />{" "}
+          </div>
           <div className="icon-circle">
             <FaLinkedinIn className="icon" />
           </div>
-          <div className="icon-circle"></div>
-          <div className="icon-circle"></div>
+          <div className="icon-circle">
+            <FaGithub />
+          </div>
+          <div className="icon-circle">
+            <TbCoffee />
+          </div>
         </div>
       </div>
     </section>
