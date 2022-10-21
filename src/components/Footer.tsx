6942c7286ad08bd0865d9195f5f8d211015a6463
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-10-21 21:59:19 +0200

    footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,16 @@
+import me from "../assets/me.png";
+
 export const Footer = () => {
-  return <div className="footer-main-container">Footer</div>;
+  return (
+    <div className="footer-main-container">
+      <img src={me} alt="" className="footer-img" />
+
+      <ul className="text-ul">
+        <li>Home</li>
+        <li>Resume</li>
+        <li>About me</li>
+        <li>Projets</li>
+      </ul>
+    </div>
+  );
 };
