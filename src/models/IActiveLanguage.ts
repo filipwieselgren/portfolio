0de2ac9d09910bdaf4ab04103c1e36254b2ede87
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-11-03 22:05:30 +0100

    text adjustment

--- src/models/IActiveLanguage.ts
+++ src/models/IActiveLanguage.ts
@@ -147,7 +147,7 @@ const heroEnglishText: IHeroText = {
       id: 3,
       question: "What is your background?",
       answer:
-        "I have a background in Saas, marketing, communication and onboarding. For the last two and a half years I have been working at the Saas company Leadoo Marketing Technologies where I onboarded clients to Leadoo's platform, helped them to implement Leadoo's solution to their website and built chabots which has affected thousands of people to get a better web experience. Please check my resume to read more about my background.",
+        "I have a background in Saas, marketing, communication and onboarding. For the last two and a half years I have been working at the Saas company Leadoo Marketing Technologies where I onboarded clients to Leadoo's platform, helped them to implement Leadoo's solution to their website and built chabots which has affected thousands of people to get a better web experience. Prior to that I worked at a digital marketing agency called Trickel where I took care of clients marketing campaigns on digital platform, such as Facbook, Instagram, Linkedin and snapchat. Please check my resume to read more about my background.",
     },
     {
       id: 4,
