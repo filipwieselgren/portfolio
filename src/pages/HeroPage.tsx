a4cb45a86b80d0971950c513faa865325c9f4d3e
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-11-15 22:48:20 +0100

    css

--- src/pages/HeroPage.tsx
+++ src/pages/HeroPage.tsx
@@ -28,7 +28,7 @@ export const HeroPage = () => {
       <Navbar scrollDown={scrollDown} toProjects={toProjects} />
       <Hero toProjects={toProjects} />
       <div ref={projectSection} id="projects" className="project-wrapper">
-        <div className="projects">Projects</div>
+        <div className="projectHeader">Projects</div>
         <Projets />
       </div>
       <div ref={contactSection} className="background-container">
