a2144a0426e416564b530dc2f4e159b680430468
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-11-17 21:06:51 +0100

    new div

--- src/components/Projets.tsx
+++ src/components/Projets.tsx
@@ -12,64 +12,69 @@ export const Projets = () => {
 
   return (
     <>
-      {languageArray.map((lang) => {
-        return lang.language === language ? (
-          lang.projects.map((project) => {
-            return (
-              <div key={project.id} className="project-card-container">
-                <div className="content-container">
-                  <img src={project.img} alt="Image on the project" />
-                  <div className="p-header-container">
-                    <h3 className="project-header">{project.name}</h3>
-                  </div>
-                  <div className="p-discription-container">
-                    <div className="project-discription">
-                      {project.description}
+      <div className="scroll">
+        {languageArray.map((lang) => {
+          return lang.language === language ? (
+            lang.projects.map((project) => {
+              return (
+                <div key={project.id} className="project-card-container">
+                  <div className="content-container">
+                    <img src={project.img} alt="Image on the project" />
+                    <div className="p-header-container">
+                      <h3 className="project-header">{project.name}</h3>
                     </div>
-                  </div>
-                  <div className="built-with-container">
-                    <div className="built-with-txt">{project.buildTxt}</div>
-                  </div>
-                  <>
-                    <div className="lang-img">
-                      {project.buildWith.map((lang) => {
-                        return (
-                          <lang.language key={lang.topicid} className="icon" />
-                        );
-                      })}
+                    <div className="p-discription-container">
+                      <div className="project-discription">
+                        {project.description}
+                      </div>
                     </div>
-                  </>
+                    <div className="built-with-container">
+                      <div className="built-with-txt">{project.buildTxt}</div>
+                    </div>
+                    <>
+                      <div className="lang-img">
+                        {project.buildWith.map((lang) => {
+                          return (
+                            <lang.language
+                              key={lang.topicid}
+                              className="icon"
+                            />
+                          );
+                        })}
+                      </div>
+                    </>
 
-                  <div className="project-btns-container">
-                    <a
-                      href={project.html_url}
-                      className="hero-link to-project-link"
-                      target="_blank"
-                    >
-                      <div className="project-btn test-app-btn">
-                        <BsCaretRight />
-                        {project.testAppBtnText}
-                      </div>
-                    </a>
-                    <a
-                      href={project.gitHub_url}
-                      className="hero-link to-project-link"
-                      target="_blank"
-                    >
-                      <div className="project-btn see-code-btn">
-                        <BsCaretRight />
-                        {project.seeCodeBtnText}
-                      </div>
-                    </a>
+                    <div className="project-btns-container">
+                      <a
+                        href={project.html_url}
+                        className="hero-link to-project-link"
+                        target="_blank"
+                      >
+                        <div className="project-btn test-app-btn">
+                          <BsCaretRight />
+                          {project.testAppBtnText}
+                        </div>
+                      </a>
+                      <a
+                        href={project.gitHub_url}
+                        className="hero-link to-project-link"
+                        target="_blank"
+                      >
+                        <div className="project-btn see-code-btn">
+                          <BsCaretRight />
+                          {project.seeCodeBtnText}
+                        </div>
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })
-        ) : (
-          <></>
-        );
-      })}
+              );
+            })
+          ) : (
+            <></>
+          );
+        })}
+      </div>
     </>
   );
 };
