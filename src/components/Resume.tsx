ba6144b1357f46189b90a6aa2f5b01f024f37536
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-11-19 11:49:18 +0100

    img

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,13 +1,25 @@
-import resume from "../assets/CV.jpg";
+import { useSelector } from "react-redux";
+import resumeEn from "../assets/CV-En.png";
+import resumeSv from "../assets/CV.SV.png";
+import { IState } from "../redux/reducers/reducer";
 import { NavBtnsHero } from "./NavBtnsHero";
 interface IScroll {
   toProjects(): void;
 }
 
 export const Resume = (props: IScroll) => {
+  const languageArray = useSelector(
+    (state: IState) => state.changeLanguage.languages
+  );
+
+  let language = useSelector((state: IState) => state.changeLanguage.value);
+
+  console.log("language:", language);
+
   return (
     <div className="resume-container">
-      <img className="resume" src={resume} alt="My resume" />
+      <img className="resume" src={resumeEn} alt="My resume" />
+      {/* <img className="resume" src={resumeSv} alt="My resume" /> */}
       <NavBtnsHero toProjects={props.toProjects} />
     </div>
   );
